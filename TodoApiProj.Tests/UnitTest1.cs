dba0020b5a5262e6a4a3f4b89983b0f4d1e3f7c6
marwan <17P8073@eng.asu.edu.eg>
2024-12-20 18:02:03 +0300

    Updating tests

--- TodoApiProj.Tests/UnitTest1.cs
+++ TodoApiProj.Tests/UnitTest1.cs
@@ -34,50 +34,6 @@ public class TodoServiceTests
     }
 
     [Fact]
-    public async Task GetAllAsync_ShouldReturnAllTodos()
-    {
-        var context = GetInMemoryDbContext();
-        var service = new TodoService(context);
-
-        await service.CreateAsync(new TodoItem { Title = "Todo 1" });
-        await service.CreateAsync(new TodoItem { Title = "Todo 2" });
-
-        var result = await service.GetAllAsync();
-
-        Assert.Equal(2, result.Count());
-    }
-
-    [Fact]
-    public async Task GetPendingAsync_ShouldReturnOnlyPendingTodos()
-    {
-        var context = GetInMemoryDbContext();
-        var service = new TodoService(context);
-
-        await service.CreateAsync(new TodoItem { Title = "Pending Todo" });
-        var completedTodo = await service.CreateAsync(new TodoItem { Title = "Completed Todo" });
-        await service.MarkCompleteAsync(completedTodo.Id);
-
-        var pendingTodos = await service.GetPendingAsync();
-
-        Assert.Single(pendingTodos);
-        Assert.Equal("Pending Todo", pendingTodos.First().Title);
-    }
-
-    [Fact]
-    public async Task MarkCompleteAsync_ShouldMarkTodoAsCompleted()
-    {
-        var context = GetInMemoryDbContext();
-        var service = new TodoService(context);
-
-        var todo = await service.CreateAsync(new TodoItem { Title = "Todo to Complete" });
-
-        var updatedTodo = await service.MarkCompleteAsync(todo.Id);
-
-        Assert.NotNull(updatedTodo);
-        Assert.True(updatedTodo.IsCompleted);
-    }
-
-    [Fact]
     public async Task MarkCompleteAsync_WithInvalidId_ShouldReturnNull()
     {
         var context = GetInMemoryDbContext();
